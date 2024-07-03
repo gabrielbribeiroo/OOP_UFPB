26b0621d289ba5766487c56306f01d31bab81102
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-02 21:26:13 -0300

    print the sequence from 1 to 10

---
+++ repeticao/Repeticao.java
@@ -0,0 +1,10 @@
+package repeticao;
+
+public class Repeticao {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i); // Print the sequence from 1 to 10
+        }
+    }
+}
+
