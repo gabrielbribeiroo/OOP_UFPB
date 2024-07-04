4f8b7f54a5a94f185a678e0107a7b36e28d74db5
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-04 14:02:52 -0300

    implementing calculator in java

--- condicionais/Operacoes.java
+++ condicionais/Operacoes.java
@@ -7,30 +7,70 @@ class Operacoes {
         Scanner scanner = new Scanner(System.in);
         
         // Reading input at the terminal
-        String input1 = scanner.nextLine();
-        String op = scanner.nextLine();
-        String input2 = scanner.nextLine();
-
-        // Changing the input to integer and putting them into the x and y variables
-        int x = Integer.parseInt(input1);
-        int y = Integer.parseInt(input2);
+        System.out.println("----------\nCALCULATOR\n----------");
+        System.out.print("Operator: ");
+        String op = scanner.nextLine(); // Reading operator
 
         // Calculating the four basic operations
         if (op.equals("+")) {
-            int sum = (int)(x + y);
-            System.out.println(x + " " + op + " " + y + " = " + sum);
+            // Reading the numbers
+            System.out.print("Number 1: ");
+            String num1 = scanner.nextLine();
+            System.out.print("Number 2: ");
+            String num2 = scanner.nextLine();
+            // Changing the input to integer and putting them into the x and y variables
+            int x = Integer.parseInt(num1);
+            int y = Integer.parseInt(num2);
+            int sum = (int)(x + y); // Calculation the sum of numbers
+            System.out.println(x + " " + op + " " + y + " = " + sum); // Print the result
         }
         else if (op.equals("-")) {
-            int sub = (int)(x - y);
-            System.out.println(x + " " + op + " " + y + " = " + sub);
+            // Reading the numbers
+            System.out.print("Number 1: ");
+            String num1 = scanner.nextLine();
+            System.out.print("Number 2: ");
+            String num2 = scanner.nextLine();
+            // Changing the input to integer and putting them into the x and y variables
+            int x = Integer.parseInt(num1);
+            int y = Integer.parseInt(num2);
+            int sub = (int)(x - y); // Calculation the subtraction of numbers
+            System.out.println(x + " " + op + " " + y + " = " + sub); // Print the result
         }
         else if (op.equals("*")) {
-            int pro = (int)(x * y);
-            System.out.println(x + " " + op + " " + y + " = " + pro);
+            // Reading the numbers
+            System.out.print("Factor 1: ");
+            String num1 = scanner.nextLine();
+            System.out.print("Factor 2: ");
+            String num2 = scanner.nextLine();
+            // Changing the input to integer and putting them into the x and y variables
+            int x = Integer.parseInt(num1);
+            int y = Integer.parseInt(num2);
+            int pro = (int)(x * y); // Calculation the multiplication of numbers
+            System.out.println(x + " " + op + " " + y + " = " + pro); // Print the result
         }
         else if (op.equals("/")) {
-            int quo = (int)(x / y);
-            System.out.println(x + " " + op + " " + y + " = " + quo);
+            // Reading the numbers
+            System.out.print("Dividing 1: ");
+            String num1 = scanner.nextLine();
+            System.out.print("Dividing 2: ");
+            String num2 = scanner.nextLine();
+            // Changing the input to integer and putting them into the x and y variables
+            int x = Integer.parseInt(num1);
+            int y = Integer.parseInt(num2);            
+            int quo = (int)(x / y); // Calculation of the integer part of the division between numbers
+            System.out.println(x + " " + op + " " + y + " = " + quo); // Print the result
+        }
+        else if (op.equals("%")) {
+            // Reading the numbers
+            System.out.print("Dividing 1: ");
+            String num1 = scanner.nextLine();
+            System.out.print("Dividing 2: ");
+            String num2 = scanner.nextLine();
+            // Changing the input to integer and putting them into the x and y variables
+            int x = Integer.parseInt(num1);
+            int y = Integer.parseInt(num2); 
+            int res = (int)(x % y); // Calculation of the remainder of the integer division between numbers      
+            System.out.println(x + " " + op + " " + y + " = " + res); // Print the result      
         }
         else {
             System.out.println("ERROR! Try again.");
@@ -38,5 +78,4 @@ class Operacoes {
 
         scanner.close(); // Close the scanner to prevent resource leak
     }
-    
 }
