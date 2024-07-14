f6f0fee97b166bf39f9d99bbd7010452ee20806f
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 18:58:47 -0300

    variable demonstration

---
+++ livros/java_para_iniciantes/Exemplo2.java
@@ -0,0 +1,16 @@
+package livros.java_para_iniciantes;
+
+public class Exemplo2 {
+    public static void main(String[] args) {
+        int var1, var2; // Declaração de variáveis
+
+        var1 = 1024; // Atribuição de valor a uma variável
+
+        System.out.println("var1 contains " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("var2 contains var / 2: ");
+        System.out.println(var2);
+    }
+}
