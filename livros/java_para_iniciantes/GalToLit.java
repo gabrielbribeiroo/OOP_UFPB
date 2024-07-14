235761ef25d61ae37be978b030db5de03ac36706
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 19:04:51 -0300

    gallons to liters conversion

---
+++ livros/java_para_iniciantes/GalToLit.java
@@ -0,0 +1,14 @@
+package livros.java_para_iniciantes;
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons; // Contém o número de galões
+        double liters; // Contém o número de litros
+        
+        gallons = 10;
+
+        liters = gallons * 3.7854; // Conversão para litros
+
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+}
