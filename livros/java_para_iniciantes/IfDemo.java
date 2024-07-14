7964eac14adb7dd143f2c6ae51b5a6c2336d19a3
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 19:09:41 -0300

    simple conditional statement

---
+++ livros/java_para_iniciantes/IfDemo.java
@@ -0,0 +1,25 @@
+package livros.java_para_iniciantes;
+
+public class IfDemo {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 2; 
+        b = 3;
+
+        if (a < b) System.out.println("a is less than b.");
+        if (a == b) System.out.println("you won't see this.");
+        System.out.println();
+
+        c = a - b; // c contém -1
+        System.out.println("c contains -1.");
+        if (c >= 0) System.out.println("c is non-negative.");
+        if (c < 0) System.out.println("c is negative.");
+        System.out.println();
+
+        c = b - a; // c contém 1
+        System.out.println("c contains 1.");
+        if (c >= 0) System.out.println("c is non-negative.");
+        if (c < 0) System.out.println("c is negative.");
+    }
+}
