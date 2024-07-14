ce226bef47d58e8d2ce7591d4652f12c52154031
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 19:02:07 -0300

    difference between int and double types

---
+++ livros/java_para_iniciantes/Exemplo3.java
@@ -0,0 +1,21 @@
+package livros.java_para_iniciantes;
+
+public class Exemplo3 {
+    public static void main(String[] args) {
+        int var; // Variável do tipo inteiro
+        double x; // Variável do tipo ponto flutuante de dupla precisão
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("Original value for var: " + var);
+        System.out.println("Original value for x: " + x);
+        System.out.println();
+
+        var = var / 4;
+        x = x / 4;
+
+        System.out.println("var after division: " + var);
+        System.out.println("x after division: " + x);
+    }
+}
