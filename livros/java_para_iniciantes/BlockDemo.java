66941c1f3c5c1195d670616cbf9b7f86f004a04f
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 21:49:16 -0300

    code block demo

---
+++ livros/java_para_iniciantes/BlockDemo.java
@@ -0,0 +1,16 @@
+package livros.java_para_iniciantes;
+
+public class BlockDemo {
+    public static void main(String[] args) {
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        if (i != 0) {
+            System.out.println("i does not equals zero.");
+            d = j / i;
+            System.out.println("j / i is " + d);
+        }
+    }
+}
