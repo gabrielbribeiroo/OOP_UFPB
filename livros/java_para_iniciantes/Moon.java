b88e52fb1a3d26b846cf14a3295932aac641bfff
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 21:58:49 -0300

    weight on the moon

---
+++ livros/java_para_iniciantes/Moon.java
@@ -0,0 +1,13 @@
+package livros.java_para_iniciantes;
+
+public class Moon {
+    public static void main(String[] args) {
+        double earth_weight; // Peso na terra
+        double moon_weight; // Peso na lua
+
+        earth_weight = 165;
+        moon_weight = earth_weight * 0.17;
+
+        System.out.println(earth_weight + " earth-pounds is equivalent to " + moon_weight + " moon-pounds.");
+    }
+}
