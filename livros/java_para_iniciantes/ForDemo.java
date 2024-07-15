d7eaf66c4e58237c9afc151701af00fff1845181
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-14 21:46:54 -0300

    repeated loop demonstration

---
+++ livros/java_para_iniciantes/ForDemo.java
@@ -0,0 +1,14 @@
+package livros.java_para_iniciantes;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        // Laço que itera 5 vezes
+        for (count=0; count<5; count++) {
+            System.out.println("This is count: " + count);
+        }
+
+        System.out.println("Done!");
+    }
+}
