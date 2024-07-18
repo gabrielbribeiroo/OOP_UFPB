331ab573d3d4913cc83ee39025314646053adef2
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-17 21:46:41 -0300

    treating char as integer

---
+++ livros/java_para_iniciantes/CharAritDemo.java
@@ -0,0 +1,21 @@
+/*
+Variáveis de caracteres podem ser tratadas como inteiros.
+*/
+
+package livros.java_para_iniciantes;
+
+public class CharAritDemo {
+    public static void main(String[] args) {
+        char ch;
+        
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++; // ch pode ser incrementada
+        System.out.println("ch now is " + ch);
+
+        ch = 90; // ch pode receber valores inteiros
+        System.out.println("ch is now " + ch);
+
+    }
+}
