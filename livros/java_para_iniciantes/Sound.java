b68eac8e166d3fb2b2596f710e1eb98c2a5867fd
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-19 15:08:59 -0300

    distance from lightning strike

---
+++ livros/java_para_iniciantes/Sound.java
@@ -0,0 +1,15 @@
+/*
+Cálculo da distância da queda de um raio cujo som leve 7,2 segundos para alcançá-lo.
+*/
+
+package livros.java_para_iniciantes;
+
+class Sound {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = 7.2 * 1100;
+
+        System.out.println("The lightning is " + dist + " feet away.");
+    }
+}
