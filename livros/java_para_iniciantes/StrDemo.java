0c3e80d74663909d6f737f27c04eb7dd9ed8b48a
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-19 15:13:53 -0300

    string demonstration

---
+++ livros/java_para_iniciantes/StrDemo.java
@@ -0,0 +1,13 @@
+/*
+Demonstração das sequências de escape em strings.
+*/
+
+package livros.java_para_iniciantes;
+
+class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("First line\nSeconde life"); // \n: gera outra linha
+        System.out.println("A\tB\tC"); // \t: tabulação (alinhamento)
+        System.out.println("D\tE\tF");
+    }
+}
