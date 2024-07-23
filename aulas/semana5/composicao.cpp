2a16d0445b2307c64d9a7b8ca78543ff3f11d35d
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-23 14:16:42 -0300

    composition

---
+++ aulas/semana5/composicao.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class motor {
+    double cilindradas;
+public:
+    motor(double c): cilindradas(c) {}
+    double get_cilindradas() {
+        return cilindradas;
+    }
+    void set_cilindradas(double c) {
+        cilindradas = c;
+    }
+};
+
+class carro {
+    int portas;
+    motor m;
+public:
+    carro(int p, motor &mm): portas(p), m(mm) {}
+    int get_portas() {
+        return portas;
+    }
+    void set_portas(int p) {
+        portas = p;
+    }
+    double get_cilindradas() {
+        return m.get_cilindradas();
+    }
+};
+
+int main(void){
+    motor m1(2.0);
+    carro c1(5, m1);
+
+    std::cout << c1.get_cilindradas() << std::endl;
+
+    return 0;
+}
