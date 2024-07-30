83da265efffc0eea8b36cc1b4d473c60dfc27647
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-07-30 20:07:44 -0300

    archive for library books

---
+++ aulas/semana6/biblioteca/livro.h
@@ -0,0 +1,33 @@
+#ifndef LIVRO_H
+#define LIVRO_H
+
+#include <string>
+#include <memory>
+#include "autor.h"
+
+using std::string;
+
+class livro {
+    string titulo;
+    int ano;
+    std::shared_ptr<autor> autor_livro; // agregação
+public:
+    livro(const string &t, int a, std::shared_ptr<autor> au);
+    ~livro();
+
+    string get_titulo();
+
+    void set_titulo(const string &t);
+
+    int get_ano();
+
+    void set_ano(int a);
+
+    std::shared_ptr<autor> get_autor();
+
+    void set_autor(std::shared_ptr<autor> au);
+
+    void print_info();
+};
+
+#endif // LIVRO_H
