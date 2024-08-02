021b09baf14eacb0fbd5118013d710e15a59b604
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-08-02 09:41:35 -0300

    motor header

---
+++ aulas/semana6/carro/motor.h
@@ -0,0 +1,18 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <string>
+
+using std::string;
+
+class motor {
+	string modelo;
+	double cilindradas;
+public:
+	motor(const string &m, double c);
+	~motor();
+
+	void ligar();
+};
+
+#endif //MOTOR_H
