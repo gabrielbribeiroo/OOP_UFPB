2885a662970cb6010938818573dec92fe599b2be
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-08-07 16:55:13 -0300

    class for books from a library

---
+++ cpp/semana7/biblioteca/livro.cpp
@@ -0,0 +1,45 @@
+#include "livro.h"
+#include <iostream>
+
+using std::cout;
+using std::string;
+using std::endl;
+
+livro::livro(const string &t, int a, std::shared_ptr<autor> au): titulo(t), ano(a), autor_livro(au) {
+    cout << "Livro: " << titulo << " criado!" << endl;
+}
+
+livro::~livro() {
+    cout << "Livro: " << titulo << " destruido!" << endl;
+}
+
+string livro::get_titulo() {
+    return titulo;
+}
+
+void livro::set_titulo(const string &t) {
+    titulo = t;
+}
+
+int livro::get_ano() {
+    return ano;
+}
+
+void livro::set_ano(int a) {
+    ano = a;
+}
+
+std::shared_ptr<autor> livro::get_autor() {
+    return autor_livro;
+}
+
+void livro::set_autor(std::shared_ptr<autor> au) {
+    autor_livro = au;
+}
+
+void livro::print_info() {
+    cout << "Titulo: " << titulo << endl;
+    cout << "Ano: " << ano << endl;
+    cout << "Autor: " << endl; 
+    autor_livro->print_info();
+}
