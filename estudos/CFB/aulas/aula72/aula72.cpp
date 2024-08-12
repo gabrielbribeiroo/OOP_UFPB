e0f702644be39dfb423faa4e9348b61cb143ec48
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-08-12 19:19:30 -0300

    iostream library

---
+++ estudos/CFB/aulas/aula72/aula72.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char n, s;
+
+    n = cin.get();
+
+    cin.ignore(80, ' ');
+
+    s = cin.get();
+
+    cout << n << " - " << s << endl;
+
+    return 0;
+}
