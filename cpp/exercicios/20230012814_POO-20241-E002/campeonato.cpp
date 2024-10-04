9ae61bc3e2aaa15988cc16112aeff2e34410ff93
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-10-03 23:47:12 -0300

    cleaning comments

--- cpp/exercicios/20230012814_POO-20241-E002/campeonato.cpp
+++ cpp/exercicios/20230012814_POO-20241-E002/campeonato.cpp
@@ -33,20 +33,6 @@ void campeonato::adicionar_jogo(const jogo &j) {
 
 // Exibe a classificação dos times no campeonato.
 void campeonato::exibir_classificacao() const {
-    /*
-    vector<time> classificacao = times;
-    
-    // Ordena os times pela pontuação, do maior para o menor
-    std::sort(classificacao.begin(), classificacao.end(), [](const time &a, const time &b) {
-        return a.get_pontuacao() > b.get_pontuacao();
-    });
-    */
-    /*
-    cout << "Tabela: " << nome_campeonato << "\n";
-    for (const auto &time : times) {
-        cout << time->get_nome() << " - " << time->get_pontuacao() << " pontos\n";
-    }
-    */
     // Faz uma cópia do vetor de times para não alterar a ordem original
     vector<time*> times_ordenados = times;
 
