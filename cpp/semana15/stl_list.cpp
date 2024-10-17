cd02808c3645276e7c700e65ada745490326d589
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-10-17 20:35:39 -0300

    stl to class list

---
+++ cpp/semana15/stl_list.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+
+using std::cout;
+using std::endl;
+using std::list;
+
+int main(void) {
+	list<int> numbers = {1, 2, 3, 4, 5};
+
+	numbers.push_back(6);
+	numbers.push_back(7);
+
+	numbers.pop_back();
+
+	for (int n : numbers) {
+		cout << n << " ";
+	}
+
+	cout << endl;
+
+    /*
+        for (int i = 0; i < numbers.size(); i++) {
+            numbers[i] *= 10;
+            cout << numbers[i] << " ";
+        }
+
+        cout << endl;
+    */
+   
+	for (list<int>::iterator it = numbers.begin(); it != numbers.end(); ++it) {
+		*it *= 10;
+		cout << *it << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
